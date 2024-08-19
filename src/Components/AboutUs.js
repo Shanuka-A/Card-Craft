5cd95e8b3a85cd877fa525d4bffa2522a6aa33ae
Shanuka Udara <it21302176@my.sliit.lk>
2024-08-19 23:21:51 +0530

    19/08/24 new update

--- src/Components/AboutUs.js
+++ src/Components/AboutUs.js
@@ -1,13 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Components/CSS/AboutUsStyle.css';
 import offer from '../Assets/3286556.jpg';
 import work from '../Assets/3857425.jpg';
 import why from '../Assets/6134172.jpg';
 import fit from '../Assets/3871751.jpg';
+import frame1 from '../Assets/Frame 1.png';
+import frame2 from '../Assets/Frame 3.png';
 
 export default function AboutUs() {
+  const [currentFrame, setCurrentFrame] = useState(0);
+
+  const frames = [frame1, frame2];
+
+  const nextFrame = () => {
+    setCurrentFrame((prevFrame) => (prevFrame + 1) % frames.length);
+  };
+
+  const prevFrame = () => {
+    setCurrentFrame((prevFrame) => (prevFrame - 1 + frames.length) % frames.length);
+  };
+
   return (
     <div className='about-container'>
+      <h1>What Can Be Included In Your Profile ?</h1>
+
+      {/* Swipeable Frame Images */}
+      <div className='swipeable-images'>
+        <button className='carousel-prev' onClick={prevFrame}>
+          <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M15.29 16.29L10.29 11.29C10.11 11.11 10 10.84 10 10.57C10 10.3 10.11 10.03 10.29 9.76L15.29 4.76L16.71 6.18L12.42 10.5L16.71 14.82L15.29 16.29Z" />
+          </svg>
+        </button>
+        <img src={frames[currentFrame]} alt={`Frame ${currentFrame + 1}`} className='frame-image' />
+        <button className='carousel-next' onClick={nextFrame}>
+          <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M8.71 16.29L13.71 11.29C13.89 11.11 14 10.84 14 10.57C14 10.3 13.89 10.03 13.71 9.76L8.71 4.76L7.29 6.18L11.58 10.5L7.29 14.82L8.71 16.29Z" />
+          </svg>
+        </button>
+      </div>
+      <br/>
+
+      <a href="https://master--glistening-cactus-f7ae79.netlify.app/" className="link-button">
+                <button className="cssbuttons-io-button">
+                    See Demo Profile
+                    <div className="icon">
+                        <svg
+                            height="24"
+                            width="24"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path d="M0 0h24v24H0z" fill="none"></path>
+                            <path
+                                d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+                                fill="currentColor"
+                            ></path>
+                        </svg>
+                    </div>
+                </button>
+            </a>
+            <br/>
+      
       {/* First Content */}
       <div className='content'>
         <img src={offer} alt="What We Offer" className='content-image' />
