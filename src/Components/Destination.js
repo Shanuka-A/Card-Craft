3d30108b41ca6d456cab0d2b164e1723e7e24cde
Shanuka Udara <it21302176@my.sliit.lk>
2024-08-28 00:48:17 +0530

    Add some cards

--- src/Components/Destination.js
+++ src/Components/Destination.js
@@ -9,6 +9,13 @@ import card5 from '../Assets/modern 2.png';
 import card6 from '../Assets/Company Logo Best Company (2).png';
 import card7 from '../Assets/modern.png';
 import card8 from '../Assets/Company Logo Best Company.png';
+import card9 from '../Assets/3B.png';
+import card10 from '../Assets/3F.png';
+import card11 from '../Assets/4B.png';
+import card12 from '../Assets/4F.png';
+import card13 from '../Assets/2B.png';
+import card14 from '../Assets/2F.png';
+
 
 class Destination extends Component {
     state = { showMoreIndex: null };
@@ -86,6 +93,54 @@ class Destination extends Component {
                     showMore={showMoreIndex === 3}
                     onToggle={() => this.handleToggle(3)}
                 />
+
+                <DesData
+                    className="first-des"
+                    heading="Basic White (Web)"
+                    price="Rs.30000.00"
+                    text="Card with black letters and white color background.
+                    We build a 5 page website for your business or other purposes with 1 year free hosting and domain.
+                    We will create a web profile without a fee. See demo profile.
+                    The front of the card will have your name and your company name.
+                    The back of the card will have your brand company and QR code.
+                    This card is the size of a credit card. (8.6cm x 5.4cm)"
+                    img1={card11}
+                    img2={card12}
+                    showMore={showMoreIndex === 2}
+                    onToggle={() => this.handleToggle(2)}
+                />
+
+                <DesData
+                    className="first-des-reverse"
+                    heading="Basic Black (Web)"
+                    price="Rs.30500.00"
+                    text="Card with black letters and white color background.
+                    We build a 5 page website for your business or other purposes with 1 year free hosting and domain.
+                    We will create a Luviroyal web profile without a fee. See Luviroyal demo profile.
+                    The front of the card will have your name and your company name.
+                    The back of the card will have your brand company and QR code.
+                    This card is the size of a credit card. (8.6cm x 5.4cm)"
+                    img1={card9}
+                    img2={card10}
+                    showMore={showMoreIndex === 3}
+                    onToggle={() => this.handleToggle(3)}
+                />
+                <DesData
+                    className="first-des"
+                    heading="Basic White (Web)"
+                    price="Rs.30000.00"
+                    text="Card with black letters and white color background.
+                    We build a 5 page website for your business or other purposes with 1 year free hosting and domain.
+                    We will create a web profile without a fee. See demo profile.
+                    The front of the card will have your name and your company name.
+                    The back of the card will have your brand company and QR code.
+                    This card is the size of a credit card. (8.6cm x 5.4cm)"
+                    img1={card13}
+                    img2={card14}
+                    showMore={showMoreIndex === 2}
+                    onToggle={() => this.handleToggle(2)}
+                />
+
             </div>
         );
     }
